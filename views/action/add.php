ad0ffdb76a8b8de5d73c3fe6f0da1d4e1154b001
Zachary Doll <Zachary.Doll@gmail.com>
2013-11-14 13:15:06 -0600

    Some file renaming

--- views/action/add.php
+++ views/action/add.php
@@ -21,35 +21,7 @@ echo $this->Form->Errors();
     <?php
     // TODO: Bundle and icon spritesheet for 2.0
     echo $this->Form->Label('CSS Icon', 'CssClass');
-    echo $this->Form->Dropdown('CssClass', array(
-        '' => 'None',
-        'ReactEverything' => 'Gear',
-        'ReactAwesome' => 'Heart',
-        'ReactFlag' => 'Flag',
-        'ReactInsightful' => 'Eye',
-        'ReactOffTopic' => 'Slashed Eye',
-        'ReactAbuse' => 'Bomb',
-        'ReactSpam' => 'Nuclear Symbol',
-        'ReactTroll' => 'Masked Smiley',
-        'ReactPromote' => 'Pointer Up',
-        'ReactDisagree' => 'Frowning Smiley',
-        'ReactAgree' => 'Smiley',
-        'ReactDislike' => 'Thumb Down',
-        'ReactLike' => 'Thumb Up',
-        'ReactDown' => 'Arrow Down',
-        'ReactUp' => 'Arrow Up',
-        'ReactWTF' => 'Surprised Smiley',
-        'ReactLOL' => 'Winking Smiley',
-        'ReactQuote' => 'Double Quote',
-        'ReactInfraction' => 'Flame',
-        'ReactAccept' => 'Checkmark',
-        'ReactReject' => 'Do Not Enter',
-        'ReactFacebook' => 'Facebook',
-        'ReactTwitter' => 'Twitter',
-        'ReactGooglePlus' => 'Google Plus',
-        'ReactMessage' => 'Envelope',
-        'ReactWarn' => 'Angry Triangle'
-    ));
+    echo $this->Form->TextBox('CssClass');
     echo Wrap('&nbsp;', 'span', array('class' => 'ReactSprite ' . $this->Form->GetValue('CssClass'), 'id' => 'icon-preview'));
     ?>
   </li>
