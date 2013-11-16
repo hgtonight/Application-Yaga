175ebe6ed7199780ca20a2cf04e18a67f1436aef
Zachary Doll <hgtonight@gmail.com>
2013-11-16 14:14:34 -0600

    Initial newbie comment rule WIP

---
+++ rules/class.newbiecomment.php
@@ -0,0 +1,68 @@
+<?php if(!defined('APPLICATION')) exit();
+include_once 'interface.yagarule.php';
+/**
+ * This rule awards badges if a comment is placed on a new member's first discussion
+ *
+ * @todo Add in checking for newness of user
+ * @author Zachary Doll
+ * @since 1.0
+ * @package Yaga
+ */
+class NewbieComment implements YagaRule{
+
+  public function Award($Sender, $User, $Criteria) {
+    $Result = FALSE;
+    switch($Criteria->Comparison) {
+      case 'gt':
+        if($User->CountComments > $Criteria->Target) {
+          $Result = TRUE;
+        }
+        break;
+      case 'lt':
+        if($User->CountComments < $Criteria->Target) {
+          $Result = TRUE;
+        }
+        break;
+      default:
+      case 'gte':
+        if($User->CountComments >= $Criteria->Target) {
+          $Result = TRUE;
+        }
+        break;
+    }
+    
+    return $Result;
+  }
+    
+  public function Form($Form) {
+    $Comparisons = array(
+        'gt' => 'more than:',
+        'lt' => 'less than:',
+        'gte' => 'more than or equal to:'        
+    );
+    
+    $String = $Form->Label('Total comments', 'CommentCount');
+    $String .= 'User has ';
+    $String .= $Form->DropDown('Comparison', $Comparisons);
+    $String .= $Form->Textbox('Target');
+    $String .= ' comments';
+
+    return $String; 
+  }
+  
+  public function Hooks() {
+    return array('CommentModel_BeforeNotification');
+  }
+  
+  public function Description() {
+    $Description = 'This rule checks a users total comment count against the criteria. If the user has more comments than the criteria, this will return true.';
+    return $Description;
+    
+  }
+  
+  public function Name() {
+    return 'CommentingComment Count Total';
+  }
+}
+
+?>
