74b37ae84c46b56cbf877f6c851366640a7aaf50
Zachary Doll <Zachary.Doll@gmail.com>
2013-11-19 16:21:57 -0600

    Fixed regression in slider update on badges/settings

--- controllers/class.badgecontroller.php
+++ controllers/class.badgecontroller.php
@@ -173,7 +173,7 @@ class BadgeController extends DashboardController {
 
     $Slider = Wrap(Wrap(Anchor($ToggleText, 'yaga/badge/toggle/' . $Badge->BadgeID, 'Hijack SmallButton'), 'span', array('class' => "ActivateSlider ActivateSlider-{$ActiveClass}")), 'td');
     $this->BadgeModel->EnableBadge($BadgeID, $Enable);
-    $this->JsonTarget('#BadgeID_' . $BadgeID . ' td:nth-child(7)', $Slider, 'ReplaceWith');
+    $this->JsonTarget('#BadgeID_' . $BadgeID . ' td:nth-child(6)', $Slider, 'ReplaceWith');
     $this->Render('Blank', 'Utility', 'Dashboard');
   }
 
