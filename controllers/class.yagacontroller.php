0ec8b6cfe0ac6f25eb7e0d1f798f01cd21128acc
Zachary Doll <Zachary.Doll@gmail.com>
2014-02-07 14:05:13 -0600

    Add baseline export functionality

--- controllers/class.yagacontroller.php
+++ controllers/class.yagacontroller.php
@@ -1,4 +1,7 @@
-<?php if(!defined('APPLICATION')) exit();
+<?php
+
+if(!defined('APPLICATION'))
+  exit();
 /* Copyright 2013 Zachary Doll */
 
 /**
@@ -29,10 +32,10 @@ class YagaController extends DashboardController {
       $this->Menu->HighlightRoute('/yaga');
     }
     $this->AddSideMenu('yaga/settings');
-    
+
     $this->AddCssFile('yaga.css');
   }
-  
+
   /**
    * Redirect to settings by default
    */
@@ -52,30 +55,30 @@ class YagaController extends DashboardController {
     $ConfigModule = new ConfigurationModule($this);
 
     $ConfigModule->Initialize(array(
-      'Yaga.Reactions.Enabled' => array(
-        'LabelCode' => 'Use Reactions',
-        'Control'   => 'Checkbox'
-      ),
-      'Yaga.Badges.Enabled' => array(
-        'LabelCode' => 'Use Badges',
-        'Control'   => 'Checkbox'
-      ),
-      'Yaga.Ranks.Enabled' => array(
-        'LabelCode' => 'Use Ranks',
-        'Control'   => 'Checkbox'
-      ),
-      'Yaga.LeaderBoard.Enabled' => array(
-        'LabelCode' => 'Show leaderboard on activity page',
-        'Control'   => 'Checkbox'
-      ),
-      'Yaga.LeaderBoard.Limit' => array(
-        'LabelCode' => 'Maximum number of leaders to show',
-        'Control'   => 'Textbox',
-        'Options'   => array(
-          'Size'  => 45,
-          'class' => 'SmallInput'
+        'Yaga.Reactions.Enabled' => array(
+            'LabelCode' => 'Use Reactions',
+            'Control' => 'Checkbox'
+        ),
+        'Yaga.Badges.Enabled' => array(
+            'LabelCode' => 'Use Badges',
+            'Control' => 'Checkbox'
+        ),
+        'Yaga.Ranks.Enabled' => array(
+            'LabelCode' => 'Use Ranks',
+            'Control' => 'Checkbox'
+        ),
+        'Yaga.LeaderBoard.Enabled' => array(
+            'LabelCode' => 'Show leaderboard on activity page',
+            'Control' => 'Checkbox'
+        ),
+        'Yaga.LeaderBoard.Limit' => array(
+            'LabelCode' => 'Maximum number of leaders to show',
+            'Control' => 'Textbox',
+            'Options' => array(
+                'Size' => 45,
+                'class' => 'SmallInput'
+            )
         )
-      )
     ));
     $this->ConfigurationModule = $ConfigModule;
 
@@ -87,10 +90,52 @@ class YagaController extends DashboardController {
     // Todo: Implement
     $this->Render('transport');
   }
-  
+
   public function Export() {
     $this->Title(T('Yaga.Export'));
-    // Todo: Implement
+    $Path = PATH_UPLOADS . DS . 'export ' . date('Y-m-d His') . '.yaga.gz';
+    $StartTime = microtime(TRUE);
+    $FileHandle = NULL;
+
+    if(function_exists('gzopen')) {
+      $FileHandle = gzopen($Path, 'wb');
+    }
+    else {
+      $FileHandle = fopen($Path, 'wb');
+    }
+
+    fwrite($FileHandle, 'Yaga Export: ' . C('Yaga.Version'));
+    fwrite($FileHandle, "\n\n");
+    fwrite($FileHandle, '// Exported Started: ' . date('Y-m-d H:i:s') . "\n\n");
+
+    $Actions = Yaga::ActionModel()->Get('Sort', 'asc');
+    $Ranks = Yaga::RankModel()->Get('Level', 'asc');
+    $Badges = Yaga::BadgeModel()->Get();
+
+    fwrite($FileHandle, "// Actions\n");
+    fwrite($FileHandle, json_encode($Actions) . "\n");
+
+    fwrite($FileHandle, "\n// Ranks\n");
+    fwrite($FileHandle, json_encode($Ranks) . "\n");
+
+    fwrite($FileHandle, "\n// Badges\n");
+    fwrite($FileHandle, json_encode($Badges) . "\n");
+
+    $EndTime = microtime(TRUE);
+    $TotalTime = $EndTime - $StartTime;
+    $m = floor($TotalTime / 60);
+    $s = $TotalTime - ($m * 60);
+
+    fwrite($FileHandle, "\n\n// Exported Completed: " . date('Y-m-d H:i:s') . sprintf(', Elapsed Time: %02d:%02.2f', $m, $s));
+
+    if(function_exists('gzopen')) {
+      gzclose($FileHandle);
+    }
+    else {
+      fclose($FileHandle);
+    }
+
     $this->Render('transport');
   }
+
 }
--- views/yaga/settings.php
+++ views/yaga/settings.php
@@ -4,3 +4,22 @@
 echo Wrap($this->Title(), 'h1');
 
 echo $this->ConfigurationModule->ToString();
+
+echo Wrap(T('Yaga.Transport'), 'h3');
+
+echo Wrap(T('Yaga.Transport.Desc'), 'div', array('class' => 'Wrap'));
+
+echo Wrap(
+        Anchor(
+                T('Import'),
+                'yaga/import',
+                array('class' => 'SmallButton')
+                ) .
+        Anchor(
+                T('Export'),
+                'yaga/export',
+                array('class' => 'SmallButton')),
+        'div',
+        array(
+            'class' => 'Wrap')
+        );
