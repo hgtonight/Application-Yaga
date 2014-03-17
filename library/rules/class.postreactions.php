773ff80e2c7e47753f6c8c9db7fe151813633d67
Zachary Doll <Zachary.Doll@gmail.com>
2014-03-17 11:19:33 -0500

    Complete PostReactions rule functionality

--- library/rules/class.postreactions.php
+++ library/rules/class.postreactions.php
@@ -1,58 +1,78 @@
 <?php if(!defined('APPLICATION')) exit();
 
 /**
- * This rule awards badges based on a user's received reactions
+ * This rule awards badges to a particular post's owner when it receives the
+ * target number of reactions.
  *
  * @author Zachary Doll
  * @since 1.0
  * @package Yaga
  */
-class ReactionCount implements YagaRule{
+class PostReactions implements YagaRule {
 
   public function Award($Sender, $User, $Criteria) {
-    $ActionID = $Sender->EventArguments['ActionID'];
-
-    if($Criteria->ActionID != $ActionID) {
-      return FALSE;
-    }
-
-    $ReactionModel = new ReactionModel();
-    $Count = $ReactionModel->GetUserCount($Sender->EventArguments['ParentUserID'], $ActionID);
-
-    if($Count >= $Criteria->Target) {
-      // Award the badge to the user that got the reaction
-      return $Sender->EventArguments['ParentUserID'];
+    $Args = $Sender->EventArguments;
+    // Check to see if the submitted action is a target
+    $Prop = 'ActionID_' . $Sender->EventArguments['ActionID'];
+    if(property_exists($Criteria, $Prop)) {
+      $Value = $Criteria->$Prop;
+      if($Value <= 0 || $Value == FALSE) {
+        return FALSE;
+      }
     }
     else {
       return FALSE;
     }
+
+    // Get the reaction counts for this parent item
+    $ReactionModel = Yaga::ReactionModel();
+    $Reactions = $ReactionModel->GetList($Args['ParentID'], $Args['ParentType']);
+
+    // Squash the dataset into an array
+    $Counts = array();
+    foreach($Reactions as $Reaction) {
+      $Counts['ActionID_' . $Reaction->ActionID] = $Reaction->Count;
+    }
+    
+    // Actually check for the reaction counts
+    foreach($Criteria as $ActionID => $Target) {
+      if($Counts[$ActionID] < $Target) {
+        return FALSE;
+      }
+    }
+
+    // The owner should be awarded
+    return $Args['ParentUserID'];
   }
 
   public function Form($Form) {
     $ActionModel = new ActionModel();
     $Actions = $ActionModel->Get();
-    $Reactions = array();
+
+    $String = $Form->Label('Yaga.Rules.PostReactions.Criteria.Head', 'ReactionCount');
+
+    $ActionList = '';
     foreach($Actions as $Action) {
-      $Reactions[$Action->ActionID] = $Action->Name;
+      $ActionList .= Wrap(sprintf(T("# of %s's:"), $Action->Name) . ' ' . $Form->Textbox('ActionID_' . $Action->ActionID, array('class' => 'SmallInput')), 'li');
     }
 
-    $String = $Form->Label('Yaga.Rules.ReactionCount.Criteria.Head', 'ReactionCount');
-    $String .= $Form->Textbox('Target', array('class' => 'SmallInput')) . ' ';
-    $String .= $Form->DropDown('ActionID', $Reactions);
+    if($ActionList == '') {
+      $String .= 'No actions defined.';
+    }
+    else {
+      $String .= Wrap($ActionList, 'ul');
+    }
 
     return $String;
   }
 
   public function Validate($Criteria, $Form) {
     $Validation = new Gdn_Validation();
-    $Validation->ApplyRules(array(
-        array(
-          'Name' => 'Target', 'Validation' => array('Required', 'Integer')
-        ),
-        array(
-          'Name' => 'ActionID', 'Validation' => array('Required', 'Integer')
-        )
-    ));
+
+    foreach($Criteria as $ActionID => $Target) {
+      $Validation->ApplyRule($ActionID, 'Integer');
+    }
+
     $Validation->Validate($Criteria);
     $Form->SetValidationResults($Validation->Results());
   }
@@ -62,11 +82,16 @@ class ReactionCount implements YagaRule{
   }
 
   public function Description() {
-    $Description = T('Yaga.Rules.ReactionCount.Desc');
+    $Description = T('Yaga.Rules.PostReactions.Desc');
     return Wrap($Description, 'div', array('class' => 'InfoMessage'));
   }
 
   public function Name() {
-    return T('Yaga.Rules.ReactionCount');
+    return T('Yaga.Rules.PostReactions');
   }
+
+  public function Interacts() {
+    return TRUE;
+  }
+
 }
