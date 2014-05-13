fcb166abb6af71d3f62dd5d57d0665ede391e1c2
Zachary Doll <hgtonight@gmail.com>
2014-05-13 13:26:13 -0500

    Add DiscussionCategory rule
    Awards a badge if the user has posted in a specific category.
    Compliments of Jan Hoos.

---
+++ library/rules/class.discussioncategory.php
@@ -0,0 +1,55 @@
+<?php if(!defined('APPLICATION')) exit();
+/**
+ * This rule awards badges if a discussion is posted in the right category
+ *
+ * @author Jan Hoos
+ * @since 1.0
+ * @package Yaga
+ */
+class DiscussionCategory implements YagaRule {
+
+  public function Award($Sender, $User, $Criteria) {
+    $Discussion = $Sender->EventArguments['Discussion'];
+    $ID = ($Discussion->CategoryID);
+    if($ID == $Criteria->CategoryID) {
+      return $Discussion->InsertUserID;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  public function Form($Form) {
+    $String  = $Form->Label('Yaga.Rules.DiscussionCategory.Criteria.Head', 'DiscussionCategory');
+    $String .= $Form->CategoryDropDown('CategoryID');
+    return $String;
+  }
+
+  public function Validate($Criteria, $Form) {
+    $Validation = new Gdn_Validation();
+    $Validation->ApplyRules(array(
+        array(
+            'Name' => 'CategoryID', 'Validation' => array('Required', 'Integer')
+        ),
+    ));
+    $Validation->Validate($Criteria);
+    $Form->SetValidationResults($Validation->Results());
+  }
+
+  public function Hooks() {
+    return array('DiscussionModel_AfterSaveDiscussion');
+  }
+
+  public function Description() {
+    return Wrap(T('Yaga.Rules.DiscussionCategory.Desc'), 'div', array('class' => 'InfoMessage'));
+  }
+
+  public function Name() {
+    return T('Yaga.Rules.DiscussionCategory');
+  }
+
+  public function Interacts() {
+    return FALSE;
+  }
+
+}
