b70326cbb2535058faa87be13b5941b3413ed201
R-J <robin.jurinka@gmx.net>
2014-05-19 20:31:28 +0200

    Create class.postcount.php
    Add a rule that awards badges based on the sum of a user's discussions & comments count

---
+++ library/rules/class.postcount.php
@@ -0,0 +1,81 @@
+<?php if(!defined('APPLICATION')) exit();
+
+/**
+ * This rule awards badges based on the sum of a user's discussions & comments count
+ *
+ * @author Zachary Doll
+ * @since 1.0
+ * @package Yaga
+ */
+class PostCount implements YagaRule{
+
+  public function Award($Sender, $User, $Criteria) {
+    $Result = FALSE;
+    $CountPosts = $User->CountDiscussions + $User->CountComments;
+    switch($Criteria->Comparison) {
+      case 'gt':
+        if($CountPosts > $Criteria->Target) {
+          $Result = TRUE;
+        }
+        break;
+      case 'lt':
+        if($CountPosts < $Criteria->Target) {
+          $Result = TRUE;
+        }
+        break;
+      default:
+      case 'gte':
+        if($CountPosts >= $Criteria->Target) {
+          $Result = TRUE;
+        }
+        break;
+    }
+
+    return $Result;
+  }
+
+  public function Form($Form) {
+    $Comparisons = array(
+        'gt' => T('More than:'),
+        'lt' => T('Less than:'),
+        'gte' => T('More than or:')
+    );
+
+    $String = $Form->Label('Yaga.Rules.PostCount.Criteria.Head', 'PostCount');
+    $String .= $Form->DropDown('Comparison', $Comparisons) . ' ';
+    $String .= $Form->Textbox('Target', array('class' => 'SmallInput'));
+
+    return $String;
+  }
+
+  public function Validate($Criteria, $Form) {
+    $Validation = new Gdn_Validation();
+    $Validation->ApplyRules(array(
+        array(
+          'Name' => 'Target', 'Validation' => array('Required', 'Integer')
+        ),
+        array(
+          'Name' => 'Comparison', 'Validation' => 'Required'
+        )
+    ));
+    $Validation->Validate($Criteria);
+    $Form->SetValidationResults($Validation->Results());
+  }
+
+  public function Hooks() {
+    return array('Gdn_Dispatcher_AppStartup');
+  }
+
+  public function Description() {
+    $Description = T('Yaga.Rules.PostCount.Desc');
+    return Wrap($Description, 'div', array('class' => 'InfoMessage'));
+  }
+
+  public function Name() {
+    return T('Yaga.Rules.PostCount');
+  }
+  
+  public function Interacts() {
+    return FALSE;
+  }
+}
