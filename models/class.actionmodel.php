hgtonight/Application-Yaga

commit 810d07ce2df80faaf1c5a9665cb6d5f6d09f6d55

Megacommit! Fixed all known inline todos

models/class.actionmodel.php
@@ -93,8 +93,8 @@ class ActionModel extends Gdn_Model {
   public function DeleteAction($ActionID, $ReplacementID = NULL) {
     if($this->ActionExists($ActionID)) {
       $this->SQL->Delete('Action', array('ActionID' => $ActionID));
-      // TODO: Ask the user if they want to delete reactions or lump them in
-      // with another action
+
+      // replace the reaction table to move reactions to a new action
       if($ReplacementID && $this->ActionExists($ReplacementID)) {
         $this->SQL->Update('Reaction')
                 ->Set('ActionID', $ReplacementID)
@@ -103,7 +103,9 @@ class ActionModel extends Gdn_Model {
       else {
         $this->SQL->Delete('Reaction', array('ActionID' => $ActionID));
       }
+      return TRUE;
     }
+    return FALSE;
   }
   
   public function SaveSort($SortArray) {

controllers/class.actioncontroller.php
@@ -68,7 +68,7 @@ class ActionController extends DashboardController {
       $this->Title(T('Yaga.EditAction'));
     }
     
-    // TODO: Autoload these, or something
+    // This is just a list of all the images in the action icons folder
     $this->SetData('Icons', array('Happy', 'Happy2', 'Smiley', 'Smiley2', 'Tongue', 'Tongue2', 'Sad', 'Sad2', 'Wink', 'Wink2', 'Grin', 'Shocked', 'Confused', 'Confused2', 'Neutral', 'Neutral2', 'Wondering', 'Wondering2', 'PointUp', 'PointRight', 'PointDown', 'PointLeft', 'ThumbsUp', 'ThumbsUp2', 'Shocked2', 'Evil', 'Evil2', 'Angry', 'Angry2', 'Heart', 'Heart2', 'HeartBroken', 'Star', 'Star2', 'Grin2', 'Cool', 'Cool2', 'Question', 'Notification', 'Warning', 'Spam', 'Blocked', 'Eye', 'Eye2', 'EyeBlocked', 'Flag', 'BrightnessMedium', 'QuotesLeft', 'Music', 'Pacman', 'Bullhorn', 'Rocket', 'Fire', 'Hammer', 'Target', 'Lightning', 'Shield', 'CheckmarkCircle', 'Lab', 'Leaf', 'Dashboard', 'Droplet', 'Feed', 'Support', 'Hammer2', 'Wand', 'Cog', 'Gift', 'Trophy', 'Magnet', 'Switch', 'Globe', 'Bookmark', 'Bookmarks', 'Star3', 'Info', 'Info2', 'CancelCircle', 'Checkmark', 'Close'));
     
     // Load up all permissions
@@ -95,18 +95,9 @@ class ActionController extends DashboardController {
         else {
           $Action = $this->ActionModel->GetNewestAction();
         }
-        $NewActionRow = Wrap(
-            Wrap(
-                    Anchor(T('Edit'), 'yaga/action/edit/' . $Action->ActionID, array('class' => 'Popup SmallButton')) . Anchor(T('Delete'), 'yaga/action/delete/' . $Action->ActionID, array('class' => 'Hijack SmallButton')), 'div', array('class' => 'Tools')) .
-            Wrap(
-                    Wrap($Action->Name, 'h4') .
-                    Wrap(
-                            Wrap($Action->Description, 'span') . ' ' .
-                            Wrap(Plural($Action->AwardValue, '%s Point', '%s Points'), 'span'), 'div', array('class' => 'Meta')) .
-                    Wrap(
-                            Wrap('&nbsp;', 'span', array('class' => 'ReactSprite React-' . $Action->ActionID . ' ' . $Action->CssClass)) .
-                            WrapIf(rand(0, 18), 'span', array('class' => 'Count')) .
-                            Wrap($Action->Name, 'span', array('class' => 'ReactLabel')), 'div', array('class' => 'Preview Reactions')), 'div', array('class' => 'Action')), 'li', array('id' => 'Action_' . $Action->ActionID));
+        
+        $NewActionRow = ActionRow($Action);
+        
         if($Edit) {
           $this->JsonTarget('#Action_' . $this->Action->ActionID, $NewActionRow, 'ReplaceWith');
           $this->InformMessage(T('Yaga.ActionUpdated'));
@@ -131,15 +122,34 @@ class ActionController extends DashboardController {
   /**
    * Remove the action via model.
    * 
-   * @todo Consider adding a confirmation page when not using JS
    * @param int $ActionID
    */
   public function Delete($ActionID) {
-    $this->Permission('Yaga.Reactions.Manage');
-    
-    $this->ActionModel->DeleteAction($ActionID);
+    $Action = $this->ActionModel->GetID($ActionID);
 
-    redirect('action/settings');
+    if(!$Action) {
+      throw NotFoundException(T('Yaga.Action'));
+    }
+
+    $this->Permission('Yaga.Reactions.Manage');
+
+    if($this->Form->IsPostBack()) {
+      if(!$this->ActionModel->DeleteAction($ActionID)) {
+        $this->Form->AddError(sprintf(T('Yaga.Error.DeleteFailed'), T('Yaga.Action')));
+      }
+
+      if($this->Form->ErrorCount() == 0) {
+        if($this->_DeliveryType === DELIVERY_TYPE_ALL) {
+          Redirect('action/settings');
+        }
+
+        $this->JsonTarget('#ActionID_' . $ActionID, NULL, 'SlideUp');
+      }
+    }
+
+    $this->AddSideMenu('badge/settings');
+    $this->SetData('Title', T('Delete Reaction'));
+    $this->Render();
   }
   
   public function Sort() {

controllers/class.badgecontroller.php
@@ -137,16 +137,34 @@ class BadgeController extends DashboardController {
   /**
    * Remove the badge via model.
    *
-   * @todo Consider adding a confirmation page when not using JS
    * @param int $BadgeID
    */
   public function Delete($BadgeID) {
+    $Badge = $this->BadgeModel->GetID($BadgeID);
+
+    if(!$Badge) {
+      throw NotFoundException(T('Yaga.Badge'));
+    }
+
     $this->Permission('Yaga.Badges.Manage');
+
+    if($this->Form->IsPostBack()) {
+      if(!$this->BadgeModel->DeleteBadge($BadgeID)) {
+        $this->Form->AddError(sprintf(T('Yaga.Error.DeleteFailed'), T('Yaga.Badge')));
+      }
+
+      if($this->Form->ErrorCount() == 0) {
+        if($this->_DeliveryType === DELIVERY_TYPE_ALL) {
+          Redirect('badge/settings');
+        }
+
+        $this->JsonTarget('#BadgeID_' . $BadgeID, NULL, 'SlideUp');
+      }
+    }
+
     $this->AddSideMenu('badge/settings');
-
-    $this->BadgeModel->DeleteBadge($BadgeID);
-
-    redirect('badge/settings');
+    $this->SetData('Title', T('Delete Badge'));
+    $this->Render();
   }
 
   /**

controllers/class.badgescontroller.php
@@ -51,8 +51,6 @@ class BadgesController extends Gdn_Controller {
     // Get list of badges from the model and pass to the view
     $this->SetData('Badges', $this->BadgeModel->GetAllBadgesUserAwards($UserID));
     
-    // TODO: Add leaderboard module
-    
     $this->Render('all');
   }
   

controllers/class.rankcontroller.php
@@ -139,16 +139,34 @@ class RankController extends DashboardController {
   /**
    * Remove the rank via model.
    *
-   * @todo Consider adding a confirmation page when not using JS
    * @param int $RankID
    */
   public function Delete($RankID) {
+    $Rank = $this->RankModel->GetID($RankID);
+
+    if(!$Rank) {
+      throw NotFoundException(T('Yaga.Rank'));
+    }
+
     $this->Permission('Yaga.Ranks.Manage');
+
+    if($this->Form->IsPostBack()) {
+      if(!$this->RankModel->DeleteRank($RankID)) {
+        $this->Form->AddError(sprintf(T('Yaga.Error.DeleteFailed'), T('Yaga.Rank')));
+      }
+
+      if($this->Form->ErrorCount() == 0) {
+        if($this->_DeliveryType === DELIVERY_TYPE_ALL) {
+          Redirect('rank/settings');
+        }
+
+        $this->JsonTarget('#RankID_' . $RankID, NULL, 'SlideUp');
+      }
+    }
+
     $this->AddSideMenu('rank/settings');
-
-    $this->RankModel->DeleteRank($RankID);
-
-    redirect('rank/settings');
+    $this->SetData('Title', T('Delete Rank'));
+    $this->Render();
   }
 
   /**

controllers/class.rulescontroller.php
@@ -8,7 +8,7 @@
  * @since 1.0
  * @package Yaga
  */
-class RulesController extends YagaController {
+class RulesController extends Gdn_Controller {
 
   /**
    * May be used in the future.

controllers/class.yagacontroller.php
@@ -4,29 +4,58 @@
 /**
  * This is the base class for controllers throughout the gamification applicati0n.
  * 
- * @todo Determine if I need this
  * @since 1.0
  * @package Yaga
  */
-class YagaController extends Gdn_Controller {
+class YagaController extends DashboardController {
 
   /**
-   * May need this in the future
-   *
-   * @access public
+   * @var array These objects will be created on instantiation and available via
+   * $this->ObjectName
    */
-  public function __construct() {
-    parent::__construct();
-  }
+  public $Uses = array('Database', 'Form');
 
   /**
-   * May use in the future
+   * Make this look like a dashboard page and add the resources
    *
    * @since 1.0
    * @access public
    */
   public function Initialize() {
-    // Call Gdn_Controller's Initialize() as well.
     parent::Initialize();
+    Gdn_Theme::Section('Dashboard');
+    if ($this->Menu) {
+      $this->Menu->HighlightRoute('/yaga/settings');
+    }
+    $this->AddJsFile('yaga.js');
+    $this->AddCssFile('yaga.css');
+  }
+
+  /**
+   * Convenience reroute to settings
+   */
+  public function Index() {
+    $this->Settings();
+  }
+  
+  /**
+   * A simple configuration page for the Yaga Application
+   */
+  public function Settings() {
+    $this->Permission('Garden.Settings.Manage');
+    
+    $ConfigModule = new ConfigurationModule($this);
+    
+    $ConfigModule->Initialize(array(
+      'Yaga.Reactions.Enabled' => array('LabelCode' => 'Use Reactions', 'Control' => 'Checkbox'),
+      'Yaga.Badges.Enabled' => array('LabelCode' => 'Use Badges', 'Control' => 'Checkbox'),
+      'Yaga.Ranks.Enabled' => array('LabelCode' => 'Use Ranks', 'Control' => 'Checkbox'),
+      'Yaga.LeaderBoard.Enabled' => array('LabelCode' => 'Show leaderboard on activity page', 'Control' => 'Checkbox'),
+      'Yaga.LeaderBoard.Limit' => array('LabelCode' => 'Maximum number of leaders to show', 'Control' => 'Textbox', 'Options' => array('Size' => 45, 'class' => 'SmallInput'))
+    ));
+    $this->AddSideMenu('yaga/settings');
+    $this->Title(T('Yaga.Settings'));
+    $this->ConfigurationModule = $ConfigModule;
+    $ConfigModule->RenderAll();
   }
 }

commit 3e59ab713f9cb19d01b8b3ec18532fe1c0a3bc4f

Fix action sorting bug
Introduced in 810d07ce2df80faaf1c5a9665cb6d5f6d09f6d55 when refactoring
the views to use functions.render.php

Thanks to DoyceT for finding this.

models/class.actionmodel.php
@@ -110,8 +110,8 @@ class ActionModel extends Gdn_Model {
 
   public function SaveSort($SortArray) {
     foreach($SortArray as $Index => $Action) {
-      // remove the 'Action_' prefix
-      $ActionID = substr($Action, 7);
+      // remove the 'ActionID_' prefix
+      $ActionID = substr($Action, 9);
       $this->SetField($ActionID, 'Sort', $Index);
     }
     return TRUE;