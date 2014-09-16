4772485ed26d32d7c0fd14cf64e10e21f641a93d
Zachary Doll <hgtonight@gmail.com>
2014-09-16 13:28:51 -0500

    Remove default reaction permission from guests

--- settings/about.php
+++ settings/about.php
@@ -3,7 +3,7 @@
 $ApplicationInfo['Yaga'] = array(
     'Name'        => 'Yet Another Gamification Application',
     'Description' => 'Yaga provides customizable reactions, badges, and ranks for your Vanilla forum software. Increase user activity by letting users react to content with emotions. Give users badges based on statistics and engagement in your community. Create and award custom badges for special events and recognition. Award Ranks which can confer different (configurable) permissions based on community perception and participation.',
-    'Version'     => '1.0.1',
+    'Version'     => '1.0.2',
 	  'Url'         => 'http://github.com/hgtonight/application-yaga',
     'Author'      => 'Zachary Doll',
     'AuthorEmail' => 'hgtonight@daklutz.com',
@@ -17,14 +17,14 @@ $ApplicationInfo['Yaga'] = array(
 
     // Application-specific permissions
     'RegisterPermissions'  => array(
-        'Yaga.Reactions.Add' => 1,    // Can a user click on reactions?
-        'Yaga.Reactions.Manage' => 0, // Can a user add/edit/delete actions?
-        'Yaga.Reactions.View' => 1,   // Can a user see the reaction record?
-        'Yaga.Reactions.Edit' => 0,   // Can a user remove other's reactions?
-        'Yaga.Badges.Add' => 0,       // Can a user give out badges?
-        'Yaga.Badges.Manage' => 0,    // Can a user adit/edit/delete badges from the system?
-        'Yaga.Badges.View' => 1,      // Can a user view badges?
-        'Yaga.Ranks.Add' => 0,        // Can a user assign ranks manually?
-        'Yaga.Ranks.Manage' => 0,     // Can a user adit/edit/delete ranks from the system?
+        'Yaga.Reactions.Add' => 'Vanilla.Comments.Add', // Can a user click on reactions?
+        'Yaga.Reactions.Manage' => 0,                   // Can a user add/edit/delete actions?
+        'Yaga.Reactions.View' => 1,                     // Can a user see the reaction record?
+        'Yaga.Reactions.Edit' => 0,                     // Can a user remove other's reactions?
+        'Yaga.Badges.Add' => 0,                         // Can a user give out badges?
+        'Yaga.Badges.Manage' => 0,                      // Can a user adit/edit/delete badges from the system?
+        'Yaga.Badges.View' => 1,                        // Can a user view badges?
+        'Yaga.Ranks.Add' => 0,                          // Can a user assign ranks manually?
+        'Yaga.Ranks.Manage' => 0,                       // Can a user adit/edit/delete ranks from the system?
     )
 );
