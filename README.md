6b30a8aa2d9497cca24e1770996f7fd808e03f8c
Zachary Doll <hgtonight@gmail.com>
2015-11-17 10:46:14 -0600

    Update reqs and licensing terms.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **Y**&#8203;et **A**&#8203;nother **G**&#8203;amification **A**&#8203;pplication is a Garden application that provides a gamification platform for Vanilla Forums and other Garden applications. It integrates by default with Vanilla Forums. Out of the box, it provides Reactions, Badges, and Ranks.
 
-It is released under the GPLv3 and may be released under a different license _**with permission**_.
+It is released under the GPLv2 and may be released under a different license _**with permission**_.
 
 
 ## Documentation
--- settings/about.php
+++ settings/about.php
@@ -4,16 +4,16 @@ $ApplicationInfo['Yaga'] = array(
     'Name'        => 'Yet Another Gamification Application',
     'Description' => 'Yaga provides customizable reactions, badges, and ranks for your Vanilla forum software. Increase user activity by letting users react to content with emotions. Give users badges based on statistics and engagement in your community. Create and award custom badges for special events and recognition. Award Ranks which can confer different (configurable) permissions based on community perception and participation.',
     'Version'     => '1.0.0-alpha',
-	  'Url'         => 'http://github.com/hgtonight/application-yaga',
+    'Url'         => 'http://github.com/hgtonight/application-yaga',
     'Author'      => 'Zachary Doll',
     'AuthorEmail' => 'hgtonight@daklutz.com',
     'AuthorUrl'   => 'http://www.daklutz.com',
-    'License'     => 'GPLv3',
+    'License'     => 'GPLv2',
     'SettingsUrl' => '/yaga/settings',
-	  'SettingsPermission' => 'Garden.Settings.Manage',
+    'SettingsPermission' => 'Garden.Settings.Manage',
 
     // Application requirements
-    'RequiredApplications' => array('Vanilla' => '2.1'),
+    'RequiredApplications' => array('Vanilla' => '2.2'),
 
     // Application-specific permissions
     'RegisterPermissions'  => array(
