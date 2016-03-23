f927fdac1950238d9b7dcf4a62d13e527ef787ff
Zachary Doll <hgtonight@gmail.com>
2016-03-23 15:58:50 -0500

    Add better action defaults

--- settings/stub.php
+++ settings/stub.php
@@ -1,35 +1,77 @@
 <?php if(!defined('APPLICATION')) exit();
-/* Copyright 2013 Zachary Doll */
-
-// Only do this once, ever.
-if(!$Drop)
-  return;
+/* Copyright 2013-2016 Zachary Doll */
 
 $SQL = Gdn::Database()->SQL();
 
-// Insert stub actions
-$SQL->Insert('Action', array(
-    'Name' => 'Like',
-    'Description' => 'I approve!',
-    'Tooltip' => 'This indicates casual approval',
-    'CssClass' => 'ReactThumbsUp',
-    'AwardValue' => 1
-));
-$SQL->Insert('Action', array(
-    'Name' => 'Dislike',
-    'Description' => 'I disapprove!',
-    'Tooltip' => 'This indicates casual disapproval',
-    'CssClass' => 'ReactThumbsUp2',
-    'AwardValue' => -1
-));
-$SQL->Insert('Action', array(
-    'Name' => 'Promote',
-    'Description' => 'This post deserves to be featured on the best of page!',
-    'Tooltip' => 'Click me if this content should be featured.',
-    'CssClass' => 'ReactPointUp',
-    'AwardValue' => 5,
-    'Permission' => 'Garden.Curation.Manage'
-));
+// Only insert stub content if nothing exists
+$Row = $SQL->get('Action', '', 'asc', 1)->firstRow(DATASET_TYPE_ARRAY);
+if (!$Row) {
+  $SQL->Insert('Action', array(
+      'ActionID' => 1,
+      'Name' => 'Promote',
+      'Description' => 'This post deserves to be featured on the best of page!',
+      'Tooltip' => 'Click me if this content should be featured.',
+      'CssClass' => 'ReactPointUp',
+      'AwardValue' => 5,
+      'Permission' => 'Garden.Curation.Manage',
+      'Sort' => 0
+  ));
+
+  $SQL->Insert('Action', array(
+      'ActionID' => 2,
+      'Name' => 'Insightful',
+      'Description' => 'This post brings new meaning to the discussion.',
+      'Tooltip' => 'Insightful',
+      'CssClass' => 'ReactEye2',
+      'AwardValue' => 1,
+      'Permission' => 'Yaga.Reactions.Add',
+      'Sort' => 1
+  ));
+
+  $SQL->Insert('Action', array(
+      'ActionID' => 3,
+      'Name' => 'Awesome',
+      'Description' => 'This post is made of pure win.',
+      'Tooltip' => 'Awesome',
+      'CssClass' => 'ReactHeart',
+      'AwardValue' => 1,
+      'Permission' => 'Yaga.Reactions.Add',
+      'Sort' => 2
+  ));
+
+  $SQL->Insert('Action', array(
+      'ActionID' => 4,
+      'Name' => 'LOL',
+      'Description' => 'This post is funny.',
+      'Tooltip' => 'LOL',
+      'CssClass' => 'ReactWink',
+      'AwardValue' => 1,
+      'Permission' => 'Yaga.Reactions.Add',
+      'Sort' => 3
+  ));
+
+  $SQL->Insert('Action', array(
+      'ActionID' => 5,
+      'Name' => 'WTF',
+      'Description' => 'This post is all sorts of shocking.',
+      'Tooltip' => 'WTF',
+      'CssClass' => 'ReactShocked',
+      'AwardValue' => 1,
+      'Permission' => 'Yaga.Reactions.Add',
+      'Sort' => 4
+  ));
+
+  $SQL->Insert('Action', array(
+      'ActionID' => 6,
+      'Name' => 'Spam',
+      'Description' => 'This post is spam.',
+      'Tooltip' => 'Spam',
+      'CssClass' => 'ReactWarning',
+      'AwardValue' => -5,
+      'Permission' => 'Garden.Curation.Manage',
+      'Sort' => 5
+  ));    
+}
 
 // Insert stub badge
 $SQL->Insert('Badge', array(
