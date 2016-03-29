54fd1d157ab4429236815c40e9c09051ef1eb2a7
Zachary Doll <hgtonight@gmail.com>
2016-03-29 10:26:07 -0500

    Make leaderboard module match the official markup

--- views/modules/leaderboard.php
+++ views/modules/leaderboard.php
@@ -6,12 +6,20 @@ echo '<h4>' . $this->Title . '</h4>';
 echo '<ul class="PanelInfo">';
 foreach($this->Data as $Leader) {
   echo Wrap(
-          UserPhoto($Leader) . ' ' .
-          UserAnchor($Leader) . ' ' .
-          Wrap(
-                  Wrap(Plural($Leader->YagaPoints, '%s Point', '%s Points'), 'span', array('class' => 'Count')),
-                  'span',
-                  array('class' => 'Aside')),
+            Anchor(
+                Wrap(
+                    Wrap(Plural($Leader->YagaPoints, '%s Point', '%s Points'), 'span', array('class' => 'Count')),
+                    'span',
+                    array('class' => 'Aside')
+                ).
+                Wrap(
+                    img(userPhotoUrl($Leader), array('class' => 'ProfilePhoto ProfilePhotoSmall')) . ' ' .
+                    Wrap($Leader->Name, 'span', array('class' => 'Username')),
+                    'span',
+                    array('class' => 'Leaderboard-User')
+                ),
+                userUrl($Leader)
+            ),
         'li');
 }
 echo '</ul>';
